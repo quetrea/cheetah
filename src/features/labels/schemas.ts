084440263370c45c6978defe574f0c68876574fd
quetrea <quetrea@hotmail.com>
2024-11-02 22:02:13 +0300

    now we are checking to the label names

--- src/features/labels/schemas.ts
+++ src/features/labels/schemas.ts
@@ -1,13 +1,27 @@
 import { z } from "zod";
 
 export const createLabelSchema = z.object({
-  label: z.string().min(1, "Required"),
+  label: z
+    .string()
+    .min(1, "Required")
+    .max(50, "Label must be at most 50 characters long")
+    .regex(
+      /^[^\s#!@#%]*$/,
+      "Label cannot contain spaces or special characters"
+    ),
   workspaceId: z.string().trim().min(1, "Required"),
   projectId: z.string().trim().min(1, "Required"),
   taskId: z.string().trim().min(1, "Required"),
 });
 
 export const updateLabelSchema = z.object({
-  label: z.string().min(1, "Required"),
+  label: z
+    .string()
+    .min(1, "Required")
+    .max(50, "Label must be at most 50 characters long")
+    .regex(
+      /^[^\s#!@#%]*$/,
+      "Label cannot contain spaces or special characters"
+    ),
   workspaceId: z.string().trim().min(1, "Required"),
 });
