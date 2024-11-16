ddd12cfd40c096db7b645ac1fd3c4cd56ff829c9
quetrea <quetrea@hotmail.com>
2024-11-16 19:56:53 +0300

    animations

--- src/components/analytics-card.tsx
+++ src/components/analytics-card.tsx
@@ -3,6 +3,8 @@ import { Card, CardHeader, CardDescription, CardTitle } from "./ui/card";
 import { cn } from "@/lib/utils";
 
 import { FaCaretDown, FaCaretUp } from "react-icons/fa";
+import { motion, animate, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
 
 interface AnalyticsCardProps {
   title: string;
@@ -20,6 +22,19 @@ export const AnalyticsCard = ({
   const increaseValueColor =
     variant === "up" ? "text-emerald-500" : "text-red-500";
   const Icon = variant === "up" ? FaCaretUp : FaCaretDown;
+
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, Math.round);
+
+  useEffect(() => {
+    const animation = animate(count, increaseValue, {
+      duration: 1,
+      ease: "easeOut",
+    });
+
+    return animation.stop;
+  }, [increaseValue]);
+
   return (
     <Card className="shadow-none border-none w-full">
       <CardHeader>
@@ -27,13 +42,16 @@ export const AnalyticsCard = ({
           <CardDescription className="flex items-center gap-x-2 font-medium overflow-hidden">
             <span className="truncate text-base">{title}</span>
           </CardDescription>
-          <div className="flex items-center gap-x-1">
+          <div className="flex items-center gap-x-1 min-w-[50px]">
             <Icon className={cn(iconColor, "size-4")} />
-            <span
-              className={cn(increaseValue, "truncate text-base font-medium")}
+            <motion.span
+              className={cn(
+                increaseValueColor,
+                "truncate text-base font-medium text-right"
+              )}
             >
-              {increaseValue}
-            </span>
+              {rounded}
+            </motion.span>
           </div>
         </div>
         <CardTitle className="text-3xl font-semibold">{value}</CardTitle>
