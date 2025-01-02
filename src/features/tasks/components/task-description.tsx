7d60deda4c8f14642eacb0fda5e75b2d70c37f47
quetrea <quetrea@hotmail.com>
2025-01-02 22:51:57 +0300

    Translation Fixes

--- src/features/tasks/components/task-description.tsx
+++ src/features/tasks/components/task-description.tsx
@@ -497,7 +497,7 @@ export const TaskDescription = ({ task }: TaskDescriptionProps) => {
               __html:
                 value ||
                 `<span class="text-muted-foreground">${t(
-                  "task.task-description.no-subtasks"
+                  "task.task-description.warning"
                 )}</span>`,
             }}
           />
