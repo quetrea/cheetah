6e3a868b338fc2e7d4143efc3705a09303f9b569
quetrea <quetrea@hotmail.com>
2025-01-14 00:52:47 +0300

    Update PomodoroClient layout: modify container styles for improved responsiveness and alignment, enhancing overall user experience

--- src/app/(dashboard)/workspaces/[workspaceId]/pomodoro/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/pomodoro/client.tsx
@@ -283,8 +283,8 @@ export const PomodoroClient = () => {
   };
 
   return (
-    <div className="max-w-7xl mx-auto p-4 sm:p-6">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 sm:gap-6">
+    <div className="max-w-7xl mx-auto flex  items-center justify-center p-4 sm:p-4 mt-2 md:mt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 sm:gap-6 ">
         <Card className="bg-white dark:bg-neutral-900 lg:min-w-[300px]">
           <div className="p-4 sm:p-6">
             <div className="bg-gray-50 dark:bg-neutral-800 rounded-lg p-3 sm:p-4">
